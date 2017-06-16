3a74172955bb545ef123365b5919061864eabb8d
cdenton87 <cdenton87@gmail.com>
2017-06-15 22:05:18 -0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
+var port = process.env.port || 1337;
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello New York\n');
-}).listen(3001);
-console.log('Server running at http://localhost:3001/');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);

3ca63a96500e255ac42f9e4f8c36dfbe52a1d5b3
cdenton87 <cdenton87@gmail.com>
2017-06-15 22:23:30 -0700

    test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ quoteMon: 1, quoteTues: 1, quoteWed: 1, quoteThurs: 1, quoteFri: 1, quoteSat: 1, quoteSun: 1 }, null, 3));
+    res.end(JSON.stringify({ quoteMon: To be or not to be, quoteTues: 1, quoteWed: 1, quoteThurs: 1, quoteFri: 1, quoteSat: 1, quoteSun: 1 }, null, 3));
 }).listen(port);

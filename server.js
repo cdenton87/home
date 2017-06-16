7de0cbdb488f6f51c6473375ebb1b433aa3d0b65
cdenton87 <cdenton87@gmail.com>
2017-06-15 22:25:49 -0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
+	 res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ quoteMon: "To be or not to be", quoteTues: 1, quoteWed: 1, quoteThurs: 1, quoteFri: 1, quoteSat: 1, quoteSun: 1 }, null, 3));
 }).listen(port);

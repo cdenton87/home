6cdf0460a99401fbaef624e230bb7be8ca5be793
cdenton87 <cdenton87@gmail.com>
2017-06-15 22:20:58 -0700

    test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ a: 1 },{ b: 1 }, null, 3));
+    res.end(JSON.stringify({ a: 1, b: 1 }, null, 3));
 }).listen(port);

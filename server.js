d13d559591886c3f082802d1b685671b321d3c94
cdenton87 <cdenton87@gmail.com>
2017-06-15 22:17:55 -0700

    test

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ a: 1 }, null, 3));
+	res.end(JSON.stringify({ b: 1 }, null, 3));
 }).listen(port);

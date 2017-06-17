7289a0e5a5ddffebe73c2cf56bdb3df13c1fd37d
cdenton87 <cdenton87@gmail.com>
2017-06-17 12:44:37 -0700

    Removed default

--- server.js
+++ server.js
@@ -66,8 +66,8 @@ http.createServer(function (req, res) {
 				res.writeHead(200, { 'Content-Type': 'text/plain' })
 				res.end("Table displayed: " + json);
 			});
-			res.end('Successfully Updated Quote');
-			return res.end();
+			//res.end('Successfully Updated Quote');
+			//return res.end();
 		});
 	}
 	/*if (req.url != "/api" || req.url != "/form" || req.url != "/uploadquote") {

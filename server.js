1737dc5005f94be735c06da0e719a8367ff601ab
cdenton87 <cdenton87@gmail.com>
2017-06-17 09:32:07 -0700

    logging

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ tableSvc.createTableIfNotExists('knightapi', function(error, result, response){
 
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
-	 res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ quoteMon: "To be or not to be", quoteTues: 1, quoteWed: 1, quoteThurs: 1, quoteFri: 1, quoteSat: 1, quoteSun: 1 }, null, 3));
+	console.log("Web Request");
 }).listen(port);

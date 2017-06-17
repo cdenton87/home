2cae7615ec07e69e9755e54808b8ee6c8673c3de
cdenton87 <cdenton87@gmail.com>
2017-06-17 11:49:12 -0700

    test

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ http.createServer(function (req, res) {
 		});
 	}
 	
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
+	//res.writeHead(200, {'Content-Type': 'text/plain'});
+	//res.end('Hello World\n');
 }).listen(port);
